c7948db42d17732fe6b2d4d48fafe6e071cef3a8
rlyost <ry@ryost.us>
2018-03-29 16:49:44 -0700

    deleted pyc files

--- twtr/models.py
+++ twtr/models.py
@@ -15,6 +15,8 @@ class Twitter_user(models.Model):
     description = models.CharField(max_length=255, null=True)
     time_zone = models.CharField(max_length=255, null=True)
     user_since = models.DateTimeField(auto_now_add=True, null=True)
+    latitude = models.DecimalField(max_digits=12, decimal_places=7, null=True)
+    longitude = models.DecimalField(max_digits=12, decimal_places=7, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
