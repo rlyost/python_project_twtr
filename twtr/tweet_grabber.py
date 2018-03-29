57cc7af51580d6cefe3d65a602e9ee494d47cc85
rlyost <ry@ryost.us>
2018-03-29 13:50:59 -0700

    Latest version

--- twtr/tweet_grabber.py
+++ twtr/tweet_grabber.py
@@ -1,6 +1,5 @@
 import twitter
 
-
 class TwitterDataSheet(object):
     api = twitter.Api(
         consumer_key='ceDtKbZWwrPWKh6iCFUlxCUEq',
@@ -8,6 +7,12 @@ class TwitterDataSheet(object):
         access_token_key='908045257484283904-JpG0pAber9n0qp1hLRpiYSqIT93VOUm',
         access_token_secret='HsEplzeY7jSvAmn4gLj42ijkSnqFIhm6dWimGZIcB9rvj',
     )
+    # api = twitter.Api(
+    #         consumer_key='XfyGfnpUqCv90OrBOxDX6g4Ga',
+    #         consumer_secret='DidKa9XdOOwvHjOqZwWFJdf1Z2ciRgmGuFyux1Tr1mhgDwG2rs',
+    #         access_token_key='305659119-GMkDSXwlGCnP2j3gT9BMMJcfUCoEJZtVqlyMSmvn',
+    #         access_token_secret='WHO2Fs9LdrZERaMrx2c4c9J2ieXALsYlrx8vFkl5ZdRdg',
+    #     )
 
     def __init__(self, screen_name):
         self.user = self.api.GetUser(screen_name=screen_name)
@@ -26,7 +31,7 @@ class TwitterDataSheet(object):
         # 'status', 'statuses_count', 'time_zone', 'url', 'utc_offset',
         # 'verified', 'withheld_in_countries', 'withheld_scope', '_json']
 
-        self.friends = self.api.GetFriends(screen_name=self.user_name)
+        self.friends = self.api.GetFriends(screen_name=screen_name)
         # This is a list of Twitter User objects
         # Each Friend in self.friends has the same attributes as above
 
@@ -49,9 +54,6 @@ class TwitterDataSheet(object):
     # - description: South Dakotan, JD/MBA PMP #veteran #airborne #ranger #Entrepreneur @vetlistus @adjutantadmn @sheepdogbook @RangerAssoc
     # - created_at: Thu May 26 15:50:21 +0000 2011
     
-
-        
-
     def find_friend_locations(self):
         '''Returns a list of a user's friends locations with the duplicates
         removed
